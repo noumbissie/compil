652fc43c194c062c8545d398facd2a96105a0ba2
noumbissie <gaelle.noumbissie@gmail.com>
2017-06-12 18:44:16 +0200

    Update testfile2.cpp

--- testfile2.cpp
+++ testfile2.cpp
@@ -11,39 +11,45 @@ using namespace std;
 
 
 // [[Rcpp::export]]
-SEXP timereadfile(const std::string & filename ) {
+SEXP timereadfile(const std::string & filename,bool header) {
   std::string data;
   std::ifstream infile;
   std::string valname;
     
   std::string colNames;
   std::string rowNames;
-  int nRow;
+  int nRow=0;
   int nCol;
   
   clock_t tStart = clock();
   infile.open(filename.c_str());
   
-  std::getline(infile,rowNames);
-  //Rcout<<lineNames<<std::endl;
+  //test existence of file 
+  if(!infile.is_open())
+    exit(EXIT_FAILURE);
+  
+  std::getline(infile,rowNames);//get first line 
 
+  //get column dimension
   nCol=std::distance(std::istream_iterator<std::string>(std::istringstream(rowNames) >> std::ws)
                              
                              ,std::istream_iterator<std::string>()
                              
                              );//on obtient la taille de l'echantillon 
 
-  //std::cout<<rowNames[3]<< std::endl;
+  //clear rownames string 
   rowNames.clear();
   
-  //getline(infile,data);
-  if(!infile.is_open())
-    exit(EXIT_FAILURE);
-  
+  //get dimension row
   while(!infile.eof()){
     getline(infile,data);
+    nRow++;
     
   }
+  if(header){nRow=nRow-1;}
+  
+  /*we need get data inside file */
+  
   infile.close();
   
   Rcpp::Rcout<<"Time taken to read file :";
